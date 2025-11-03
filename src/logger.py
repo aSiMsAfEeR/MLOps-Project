b4f9c08c94704599b245fc23873f9a09459f9097
AasimDev <158428327+aSiMsAfEeR@users.noreply.github.com>
2025-11-03 14:01:55 +0500

    update

--- src/logger.py
+++ src/logger.py
@@ -2,30 +2,24 @@ import logging
 import os
 
 def get_logger(name):
-    # Determine the absolute path to the logs directory
     log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
     os.makedirs(log_dir, exist_ok=True)
 
     log_file = os.path.join(log_dir, "app.log")
 
-    # Create logger
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    # File handler
     file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(logging.INFO)
 
-    # Console handler
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
-    # Formatter
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     file_handler.setFormatter(formatter)
     console_handler.setFormatter(formatter)
 
-    # Prevent duplicate handlers
     if not logger.handlers:
         logger.addHandler(file_handler)
         logger.addHandler(console_handler)
